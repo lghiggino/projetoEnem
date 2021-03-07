4b94aca0d16467599375c66584633094ce328919
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-06 21:20:58 -0300

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+import footerStyle from '../styles/Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={footerStyle.footer}>
+            <ul>
+                <li>
+                    <Link href="http://www.github.com/lghiggino">Project made by Leonardo Ghiggino</Link>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About Web Dev Newz</title>
+                <meta name="keywords" content="about web dev news"/>
+            </Head>
+
+            <h1>About this</h1>
+        </div>
+
+    )
+}
+
+export default about
